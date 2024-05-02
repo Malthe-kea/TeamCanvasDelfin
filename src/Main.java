2b92a6096c32ef783da97a9a4b61508cdbedf7cd
sonnekea <soso0002@stud.kea.dk>
2024-05-02 09:11:19 +0200

    made calculateprtest

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,15 @@
 import database.UserDB;
+import user_domain.CompetitiveMember;
 
 public class Main {
     public static void main(String[] args) {
         UserDB db = new UserDB();
+
+
+        //Laver en instans for at teste om den printer dataen korrekt
+        CompetitiveMember member = new CompetitiveMember(true, true, 25, false);
+        //Kaller calculatePR metoden
+        member.calculatePR();
 
     }
 }
