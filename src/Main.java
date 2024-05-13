b4310138c6bd8273e85ef2aca73145b3de52125c
Noah8282 <nova0001@stud.kea.dk>
2024-05-13 09:48:09 +0200

    Removed Esra Userinterface

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,4 @@
-import database.DBController;
-import database.competition_style_DB.CompetitionDB;
-import database.competition_style_DB.StyleDB;
-import database.rowNameEnum.CompetitiveMemberDBRowNames;
-import database.rowNameEnum.DBRowNames;
-import database.userDB.CompetitiveMemberDB;
-import database.userDB.UserDB;
-import domain_model.Processors.TreasurerProcessor;
 import domain_model.UserInterface;
-import user_domain.CompetitiveMember;
-import user_domain.Member;
-import user_domain.User;
-import user_domain.competition.Competition;
-import user_domain.competition.Style;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
