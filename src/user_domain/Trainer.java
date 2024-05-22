135ac73356feb8f7e95dcf4e484c6166bcc367ad
Noah Van Wagenen <nova0001@stud.kea.dk>
2024-05-22 08:34:16 +0200

    Removed redunant

--- src/user_domain/Trainer.java
+++ src/user_domain/Trainer.java
@@ -6,8 +6,6 @@ import java.util.Collections;
 public class Trainer extends User {
     private ArrayList<CompetitiveMember> competitiveMemberList;
     private boolean isSeniorTrainer;
-    private ArrayList<SortExampleTest> sortingExample;
-
 
     public Trainer(int userID, String firstName, String lastName, boolean isSeniorTrainer) {
         super(userID, firstName, lastName);
